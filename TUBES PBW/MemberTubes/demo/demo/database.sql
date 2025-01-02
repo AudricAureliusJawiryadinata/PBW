f094ca6f6cb597913859ef3aa98d438252bd1be7
AudricAureliusJawiryadinata <6182101039@student.unpar.ac.id>
2025-01-02 21:05:05 +0700

    database terbaru (penambahan tabel admin)

--- TUBES PBW/MemberTubes/demo/demo/database.sql
+++ TUBES PBW/MemberTubes/demo/demo/database.sql
@@ -1,8 +1,10 @@
 select * from artis
 select * from member
+select * from admin
 
 DROP TABLE IF EXISTS member;
 DROP TABLE IF EXISTS artis;
+DROP TABLE IF EXISTS admin;
 CREATE TABLE member (
     id SERIAL PRIMARY KEY,
     username(100) NOT NULL,
@@ -14,6 +16,11 @@ CREATE TABLE artis (
     nama_artis VARCHAR(100) NOT NULL,
     genre_musik VARCHAR(50) NOT NULL
 );
+CREATE TABLE admin (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
 INSERT INTO artis (nama_artis, genre_musik)
 VALUES
     ('Taylor Swift', 'Pop'),
@@ -21,3 +28,6 @@ VALUES
     ('Metallica', 'Metal'),
     ('Adele', 'Soul'),
     ('BTS', 'K-Pop');
+INSERT INTO admin (username, password)
+VALUES
+    ('audric', '12345');
