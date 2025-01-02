890864c2441e739077fb8a9d86cee0a15cb395c3
AudricAureliusJawiryadinata <6182101039@student.unpar.ac.id>
2025-01-02 21:23:52 +0700

    database baru

--- TUBES PBW/MemberTubes/demo/demo/database.sql
+++ TUBES PBW/MemberTubes/demo/demo/database.sql
@@ -5,6 +5,9 @@ select * from admin
 DROP TABLE IF EXISTS member;
 DROP TABLE IF EXISTS artis;
 DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS show;
+DROP TABLE IF EXISTS setlist;
+
 CREATE TABLE member (
     id SERIAL PRIMARY KEY,
     username(100) NOT NULL,
@@ -31,3 +34,30 @@ VALUES
 INSERT INTO admin (username, password)
 VALUES
     ('audric', '12345');
+
+CREATE TABLE show (
+    id SERIAL PRIMARY KEY,
+    nama_show VARCHAR(100) NOT NULL
+);
+
+INSERT INTO show (nama_show)
+VALUES
+    ('Show 1'),
+    ('Show 2');
+
+
+CREATE TABLE setlist (
+    id SERIAL PRIMARY KEY,
+    nama_lagu VARCHAR(100) NOT NULL,
+	show_terkait VARCHAR(100) NOT NULL,
+    show_id INT NOT NULL,
+    FOREIGN KEY (show_id) REFERENCES show (id)
+);
+
+INSERT INTO setlist (nama_lagu, show_terkait, show_id)
+VALUES
+    ('Shape of You', 'Show 1',1),
+    ('Rolling in the Deep', 'Show 2',2),
+    ('Nothing Else Matters', 'Show 1',1),
+    ('Butter', 'Show 2',2),
+    ('Someone Like You', 'Show 1',1);
