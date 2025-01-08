bd069ce450986c94517de0f5bea9efaa9472f7bd
AudricAureliusJawiryadinata <6182101039@student.unpar.ac.id>
2025-01-08 20:56:51 +0700

    delete folder

--- TUBES PBW/MemberTubes/demo/demo/src/main/java/com/example/demo/Setlist/SetListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Setlist;
-
-import java.util.List;
-
-public interface SetListRepository {
-    void addSetlist(String namaLagu, String showTerkait, int showId); // Tambah Setlist
-    List<Setlist> findSetlistsByShowId(int showId); // Ambil Setlist berdasarkan ID Show
-}
--- TUBES PBW/MemberTubes/demo/demo/src/main/java/com/example/demo/Show/ShowRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Show;
-
-import java.util.List;
-
-public interface ShowRepository {
-    void addShow(String namaShow); // Tambah Show
-    List<Show> findAllShows(); // Ambil semua Show
-}
